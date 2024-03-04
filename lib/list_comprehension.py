fe6f7eb655f6e65094ec2fa52412a97a3c7a4655
Marvin <okongomarvin971@gmail.com>
2024-03-04 09:36:33 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_numlist = [n for n in num_list if n%2 == 0]
+    return new_numlist
 
 def make_exclamation(sentence_list):
-    pass
+    new_sentence_list = [n + '!' for n in sentence_list]
+    return new_sentence_list
